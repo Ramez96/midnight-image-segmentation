268e1f76154dd167fcf2245acf67f6b6682c15fb
Christian Weiss <waverock2@web.de>
2024-12-04 16:49:37 +0800

    preprocessings

--- pre_processing_1.py
+++ pre_processing_1.py
@@ -1,16 +1,5 @@
 
+
+
 def gaussian_filter():
     pass
-
-def laplacian_filter():
-    pass
-
-def contrast_stretching():
-    pass
-
-
-def approach():
-    gaussian_filter()
-    laplacian_filter()
-    contrast_stretching()
-    
--- pre_processing_3.py
+++ pre_processing_3.py
@@ -1,4 +1,5 @@
 
+
 def median_filter():
     pass
 
