707719721ad8c71af9590c225c6c4ded5e86bb77
Christian Weiss <waverock2@web.de>
2024-12-04 17:10:37 +0800

    gaussian filter

--- pre_processing_1.py
+++ pre_processing_1.py
@@ -1,5 +1,10 @@
+import cv2
+import os
 
 
-
-def gaussian_filter():
-    pass
+folder_path = "stanford_background_dataset/images"
+file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+for file_name in file_list:
+    image = cv2.imread(f"stanford_background_dataset/images/{file_name}")
+    gaussian_blurred = cv2.GaussianBlur(image, (5, 5), 1.0)
+cv2.imwrite(f"stanford_background_dataset/preprocessing1/{file_name}", gaussian_blurred)
