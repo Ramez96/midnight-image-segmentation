9dec4b4e358f67df460d1b54ed87a0740fa898e0
Ramez <ramri882@student.liu.se>
2024-12-13 00:41:23 +0800

    Gaussian blur

--- preprocessing/pre_processing_1.py
+++ preprocessing/pre_processing_1.py
@@ -5,25 +5,25 @@ def gaussian(image):
     return cv2.GaussianBlur(image, (11, 11), 2.0)
 
 # gaussian blur
-folder_path = "../dataset/test/images"
+folder_path = "../dataset/test/preprocessing3"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/test/images/{file_name}")
-    gaussian_blurred = gaussian(image)
-    cv2.imwrite(f"../dataset/test/preprocessing1/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = gaussian(image)
+    cv2.imwrite(f"../dataset/test/preprocessing1/{file_name}", filtered_image)
 
 # gaussian blur
-folder_path = "../dataset/val/images"
+folder_path = "../dataset/val/preprocessing3"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/val/images/{file_name}")
-    gaussian_blurred = gaussian(image)
-    cv2.imwrite(f"../dataset/val/preprocessing1/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = gaussian(image)
+    cv2.imwrite(f"../dataset/val/preprocessing1/{file_name}", filtered_image)
 
 # gaussian blur
-folder_path = "../dataset/train/images"
+folder_path = "../dataset/train/preprocessing3"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/train/images/{file_name}")
-    gaussian_blurred = gaussian(image)
-    cv2.imwrite(f"../dataset/train/preprocessing1/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = gaussian(image)
+    cv2.imwrite(f"../dataset/train/preprocessing1/{file_name}", filtered_image)
