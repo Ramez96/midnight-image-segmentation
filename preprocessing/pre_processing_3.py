ca59aa6ca5bae0980e7f22e4266bd058663f6bb7
Ramez <ramri882@student.liu.se>
2024-12-13 00:41:45 +0800

    Noise function

--- preprocessing/pre_processing_3.py
+++ preprocessing/pre_processing_3.py
@@ -4,43 +4,33 @@ import numpy as np
 from scipy.signal import wiener
 
 
-def apply_wiener_rgb(image, mysize=1, noise=1e-3):
+def so_noisy(image, mean=0, var=0.001):
     # Create an empty image to store the Wiener filtered result
-    wiener_image = np.zeros_like(image, dtype=np.float32)
-
-    # Apply the Wiener filter to each channel
-    for i in range(3):  # 0=Red, 1=Green, 2=Blue
-        channel = image[..., i]
-        # Apply SciPy Wiener filter with noise regularization to avoid division by zero
-        wiener_image[..., i] = wiener(channel, mysize=mysize, noise=noise)
-
-    # Convert NaNs and Infs to valid values (e.g., zero or max 255)
-    wiener_image = np.nan_to_num(wiener_image, nan=0.0, posinf=255, neginf=0)
-
-    # Clip the values to valid range [0, 255] and convert back to uint8
-    wiener_image = np.clip(wiener_image, 0, 255).astype(np.uint8)
-
-    return wiener_image
+    row, col, ch = image.shape
+    sigma = var ** 0.5
+    gauss = np.random.normal(mean, sigma, (row, col, ch))
+    noisy_image = np.clip(image + gauss * 255, 0, 255).astype(np.uint8)
+    return noisy_image
 
 
 # gaussian blur
 folder_path = "../dataset/test/images"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/test/images/{file_name}")
-    gaussian_blurred = apply_wiener_rgb(image)
-    cv2.imwrite(f"../dataset/test/preprocessing3/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = so_noisy(image)
+    cv2.imwrite(f"../dataset/test/preprocessing3/{file_name}", filtered_image)
 
 folder_path = "../dataset/train/images"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/train/images/{file_name}")
-    gaussian_blurred = apply_wiener_rgb(image)
-    cv2.imwrite(f"../dataset/train/preprocessing3/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = so_noisy(image)
+    cv2.imwrite(f"../dataset/train/preprocessing3/{file_name}", filtered_image)
 
 folder_path = "../dataset/val/images"
 file_list = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 for file_name in file_list:
-    image = cv2.imread(f"../dataset/val/images/{file_name}")
-    gaussian_blurred = apply_wiener_rgb(image)
-    cv2.imwrite(f"../dataset/val/preprocessing3/{file_name}", gaussian_blurred)
+    image = cv2.imread(f"{folder_path}/{file_name}")
+    filtered_image = so_noisy(image)
+    cv2.imwrite(f"../dataset/val/preprocessing3/{file_name}", filtered_image)
